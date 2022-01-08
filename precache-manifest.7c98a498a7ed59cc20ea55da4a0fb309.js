bce7821e48cf30905dae88c2a5fb4426e47ee07a
yxgdev <yxg.devs@gmail.com>
2022-01-09 00:54:14 +0800

    Updates

---
+++ precache-manifest.7c98a498a7ed59cc20ea55da4a0fb309.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9867d4db952864e7abc0d2e409c70277",
+    "url": "/pomodorov-v1/index.html"
+  },
+  {
+    "revision": "6022df0a09d04a498eb4",
+    "url": "/pomodorov-v1/static/css/main.5bad3441.chunk.css"
+  },
+  {
+    "revision": "e82509182996123def5e",
+    "url": "/pomodorov-v1/static/js/2.491255c5.chunk.js"
+  },
+  {
+    "revision": "eaa35b1ccc0320d7ec9314f2e19ded2a",
+    "url": "/pomodorov-v1/static/js/2.491255c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6022df0a09d04a498eb4",
+    "url": "/pomodorov-v1/static/js/main.a29994ac.chunk.js"
+  },
+  {
+    "revision": "d0e497c7c5b9721d47d7",
+    "url": "/pomodorov-v1/static/js/runtime-main.18a5d112.js"
+  },
+  {
+    "revision": "fadd6c3643f7d43cfb47f84f7a24ec0f",
+    "url": "/pomodorov-v1/static/media/TomatoTimer.fadd6c36.png"
+  },
+  {
+    "revision": "e4ea310764b1975a3fa2b06143187115",
+    "url": "/pomodorov-v1/static/media/old-clock.e4ea3107.wav"
+  }
+]);
